7004f307690fd1d08b2dbf099809c636ab34ce9e
Dominic Radtke <develop@radtke-ruiz.de>
2023-04-03 19:03:42 +0200

    add hashr form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hashr from "./components/Hashr";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <div class="container mx-auto">
       <Hero />
+      <Hashr />
     </div>
   );
 }
---
+++ src/components/Hashr.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Hashr extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <div class="mb-6">
+                        <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website</label>
+                        <input type="website" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="google.com" required />
+                    </div>
+                    <div class="mb-6">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Master Password</label>
+                        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+                    </div>
+                    <div class="mb-6">
+                        <label for="modificator" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modificator</label>
+                        <input type="number" id="modificator" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Hashr;
