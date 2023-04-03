8cb56a4bc94bce6846092cb8173b563fd20386cd
Dominic Radtke <develop@radtke-ruiz.de>
2023-04-03 19:52:16 +0200

    remove tailwind components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: 'media',
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ]
 }
 
