f7eac6d5d8c0b93432ff169e1de7b4400d21d967
Dominic Radtke <develop@radtke-ruiz.de>
2023-04-03 19:26:27 +0200

    add basic layout and enable dark-mode

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div class="container mx-auto">
-      <Hero />
-      <Hashr />
+    <div class="bg-white dark:bg-slate-800 min-h-screen">
+      <div class="max-w-screen-lg mx-auto">
+        <div class="p-8">
+          <Hero />
+          <Hashr />
+        </div>
+      </div>
     </div>
   );
 }
