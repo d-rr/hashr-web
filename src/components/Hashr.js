360d7097e7360696abd463415f88093cdf4e2d3e
Dominic Radtke <develop@radtke-ruiz.de>
2023-04-04 19:29:06 +0200

    add copy to clipboard functionality

--- src/components/Hashr.js
+++ src/components/Hashr.js
@@ -30,6 +30,12 @@ function Hashr() {
         }
     }
 
+    function copyToClipboard(event) {
+        navigator.clipboard.writeText(hashedPassword)
+        console.log("password copied to clipboard")
+        event.preventDefault()
+    }
+
     return (
         <div>
             <form>
@@ -39,7 +45,6 @@ function Hashr() {
                         type="url"
                         id="website"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="google.com"
                         required
                         onChange={updateHashedPassword}
                         ref={websiteRef} />
@@ -50,7 +55,6 @@ function Hashr() {
                         type="password"
                         id="master_password"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="•••••••••"
                         required
                         onChange={updateHashedPassword}
                         ref={masterPasswordRef} />
@@ -68,14 +72,23 @@ function Hashr() {
                         onChange={updateHashedPassword}
                         ref={modifierRef} />
                 </div>
-                <div className="mb-6">
-                    <input
-                        type="text"
-                        id="hashed_password"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value={hashedPassword}
-                        readOnly
-                        disabled />
+                <div className="grid gap-6 mb-6 md:grid-cols-2">
+                    <div>
+                        <input
+                            type="text"
+                            id="hashed_password"
+                            value={hashedPassword}
+                            readOnly
+                            disabled />
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            name="copyButton"
+                            onClick={copyToClipboard}
+                        >Copy to clipboard</button>
+                    </div>
                 </div>
             </form>
         </div>
