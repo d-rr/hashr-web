0c22ed2a844820331241afa66938ee69e66f9723
Dominic Radtke <develop@radtke-ruiz.de>
2023-04-07 10:18:31 +0200

    remove zoom and scale ability

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { Helmet } from "react-helmet";
 import Hashr from "./components/Hashr";
 import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="bg-white dark:bg-slate-800 min-h-screen">
-      <div className="max-w-screen-lg mx-auto">
-        <div className="p-8">
-          <div className="mb-8">
-            <Hero />
-          </div>
-          <div className="my-8">
-            <Hashr />
+    <div>
+      <Helmet>
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+      </Helmet>
+      <div className="bg-white dark:bg-slate-800 min-h-screen">
+        <div className="max-w-screen-lg mx-auto">
+          <div className="p-8">
+            <div className="mb-8">
+              <Hero />
+            </div>
+            <div className="my-8">
+              <Hashr />
+            </div>
           </div>
         </div>
       </div>
